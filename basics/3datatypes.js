5e00d89c3a5af72826ebb960bd2aa15f07e0be85
JatinChoudhary1712 <159408843+JatinChoudhary1712@users.noreply.github.com>
2024-08-31 07:55:58 +0000

    day3

---
+++ basics/3datatypes.js
@@ -0,0 +1,3 @@
+"use strict" ;  //treat all JS code as a newer version
+// alert("hello")  we using nodejs not browser that's why give eror for alert 
+ 
---
+++ basics/4conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "fhubw";
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 , fasle => 0
+
+let isLoggedIn = "jatin" 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1   => true
+// "" => false
+//"jatin" => true
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
