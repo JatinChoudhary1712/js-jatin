832095ffb4e5571d041b0c56c5a183c2fbc007a2
JatinChoudhary1712 <159408843+JatinChoudhary1712@users.noreply.github.com>
2024-08-31 07:08:01 +0000

    day2

---
+++ basics/2variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "jatinchoudhary025@gmail.com";
+var accountPass = "12345";   //prefer not to use var becasue of issue in block scope and function scope
+accountCity = "Chandigarh";
+let accontstate 
+// // accountId = 2;   // not allowed
+console.log(accountId);
+ accountEmail = "jatuin@gmail.com";
+ accountPass = 21213221;
+ accountCity = "bengaluru";
+
+ console.table([accountId , accountEmail , accountCity , accountPass , accontstate]);
+   
