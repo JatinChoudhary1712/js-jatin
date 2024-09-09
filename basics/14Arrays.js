dbc847601eae09bcae8288e0164d1330ec68ad8f
JatinChoudhary1712 <159408843+JatinChoudhary1712@users.noreply.github.com>
2024-09-09 18:05:09 +0000

    arrays1

---
+++ basics/14Arrays.js
@@ -0,0 +1,17 @@
+// arrays
+
+const myArr = [0,3,3,2,4,3,4]
+const myheros = ["shaktiman" , "nagaraj"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[2]);
+
+// arrays method
+// myArr.push(3)
+// myArr.push(4)
+// myArr.pop()
+// console.log(myArr);
+
+
+myArr.unshift(10)
+myArr.shift(9)
+console.log(myArr);
